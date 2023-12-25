a99a84eeab68e713d00a5a695bda569f2a520b06
Harinash <prohari35@gmail.com>
2023-12-25 18:37:38 +0800

    test

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+  <>
+    <Header />
+    <div> 
+      <h1>Harinash</h1>
+    </div>
+    <Footer />
+  </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from "../assets/react.svg"
+
+export const Header = () => {
+  return (
+    // <div>Header</div>
+    <div>
+        <img className="logo" src={Logo} alt="" />
+    </div>
+  )
+}
